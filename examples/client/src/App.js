3dfb320a8a324ed47217880e1847f8d0ce8f5ef2
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-10-30 22:20:44 +0700

    feat: Working e2e

--- examples/client/src/App.js
+++ examples/client/src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { request } from 'graphql-request'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  // When view is ready
+  componentDidMount = async () => {
+    // We'll make request to this endpoint
+    const uri = 'http://localhost:4000/graphql'
+
+    // With this query
+    const query = `
+    {
+      getBalanceInEth(addr: "0x62dfc32cd1a3883f1de8f3a3c96105638912bdd9") {
+        value {
+          string
+        }
+      }
+    }
+    `
+
+    // Now we fetch data...
+    const { getBalanceInEth } = await request(uri, query).catch(err => console.error(err))
+    const eth = getBalanceInEth.value.string
+
+    // Then set state
+    this.setState({ eth })
+  }
+
+  render () {
+    // loading
+    if (!this.state.eth) return <p>loading...</p>
+
+    // success
+    return <p>getBalanceInEth : {this.state.eth}</p>
   }
 }
 
-export default App;
+export default App
--- examples/server/index.js
+++ examples/server/index.js
@@ -9,6 +9,10 @@ const url = 'http://localhost:8545'
 const { genGraphQlProperties } = require('../../lib')
 const { schema, rootValue } = genGraphQlProperties({ artifacts: [MetaCoinArtifact], provider: { url }, graphql })
 
+// CORS
+const cors = require('cors')
+app.use(cors())
+
 app.use(
   '/graphql',
   graphqlHTTP({
